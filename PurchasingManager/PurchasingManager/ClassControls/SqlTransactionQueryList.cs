ee8a6f8269c545252fe7394c4b21dfdc5ac74b82
NDCongSp <ndcong08cddv02@gmail.com>
2022-02-12 12:01:52 +0700

    co ban 1

---
+++ PurchasingManager/PurchasingManager/ClassControls/SqlTransactionQueryList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchasingManager
+{
+    public class SqlTransactionQueryList
+    {
+        private string query;
+        private object[] parametter;
+
+        public string Query { get => query; set => query = value; }
+        public object[] Parametter { get => parametter; set => parametter = value; }
+    }
+}
