4f556bd48b0077afea7e13a28f6aca1a8a22aa90
atolucky6 <61114829+atolucky6@users.noreply.github.com>
2022-02-22 11:24:25 +0700

    add base view

---
+++ PurchasingManager/QuanLyThuMua/Views/ucKhachHang.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace QuanLyThuMua
+{
+    partial class ucKhachHang
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ucKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Name = "ucKhachHang";
+            this.Size = new System.Drawing.Size(787, 461);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
