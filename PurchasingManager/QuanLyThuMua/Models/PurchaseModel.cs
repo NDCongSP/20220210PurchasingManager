290f9781655953f0cd17ba0f43230ac3bf90f028
NDCongSp <ndcong08cddv02@gmail.com>
2022-03-04 21:10:24 +0700

    update thu mua

--- PurchasingManager/QuanLyThuMua/Models/PurchaseModel.cs
+++ PurchasingManager/QuanLyThuMua/Models/PurchaseModel.cs
@@ -14,21 +14,32 @@ namespace QuanLyThuMua
         [DesignOnly(true)]
         [Browsable(false)]
         public int CustomerId { get; set; }
+        [Browsable(false)]
+        public string  Phone { get; set; }
+        [Browsable(false)]
+        public string Address { get; set; }
+        [Browsable(false)]
+        public string PathFileExcelOpen { get; set; }
+        [Browsable(false)]
+        public int MuType { get; set; }
+
+        [DisplayName("Tên khách hàng")]
+        public string  Name { get; set; }
         [DisplayName("Ngày tạo")]
         public DateTime CreatedDate { get; set; }
         [DisplayName("Loại")]
         public string Type { get; set; }
         [DisplayName("Trọng lượng")]
         public double Weight { get; set; }
-        [DesignOnly(true)]
-        [Browsable(true)]
+        [Browsable(false)]
         public int PriceId { get; set; }
         [DisplayName("Đơn giá")]
         public double Price { get; set; }
         [DisplayName("Thanh toán ngay")]
         public int PayNow { get; set; }
+      
         [DisplayName("Loại mủ")]
-        public int MuType { get; set; }
+        public string MuTypeName { get; set; }
         [DisplayName("Số độ")]
         public double? Degree { get; set; }
         [DisplayName("Lưu ý")]
